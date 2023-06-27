30b91e54d03fd30df80b82715430d9d958286815
Baker357 <133279040+Baker357@users.noreply.github.com>
2023-06-27 14:15:26 -0400

    Update scripts.js
    Add array for pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-alert('Hello World');
-console.log ('hello');
-let favoriteFood ='pizza';
-document.write(favoriteFood)
+let pokemonList =[
+{name:"Bulbasaur", types:["grass","poison"],hieght:".7m" },
+{name:"Ivysaur", types:["grass","poison" ], hieght:"1m" },
+{name:"Venusaur", types:["grass","poison"] , hieght:"2m" },
+{name:"Charmander", types:"fire" , hieght:".6m" },
+{name:"Charmeleon", types:"fire" , hieght:"1.1m" },
+{name:"Charizard", types:["fire","flying" ], hieght:"1.7m" }]
