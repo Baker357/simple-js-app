7c625c6c2af7f6c46dddd30e6974fbe31e8eb178
Baker357 <133279040+Baker357@users.noreply.github.com>
2023-12-05 15:10:13 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,32 @@
-let pokemonRepository = (function () {
-    let pokemonList = [];
-  
-    function add(pokemon) {
-      pokemonList.push(pokemon);
-    }
-  
-    function getAll() {
-      return pokemonList;
-    }
-  
-    return {
-      add: add,
-      getAll: getAll
-    };
-  })();
+document.write("hello");
 
-let pokemonList =[
-{name:"Bulbasaur", types:["grass"," poison"],hieght:".7" },
-{name:"Ivysaur", types:["grass"," poison" ], hieght:"1" },
-{name:"Venusaur", types:["grass"," poison"] , hieght:"2" },
-{name:"Charmander", types:"fire" , hieght:".6" },
-{name:"Charmeleon", types:"fire" , hieght:"1.1" },
-{name:"Charizard", types:["fire"," flying" ], hieght:"1.7" }]
+let pokemonRepository = pokemonList = [
+  {name:"Bulbasaur", types:["grass"," poison"],hieght:".7" },
+  {name:"Ivysaur", types:["grass"," poison" ], hieght:"1" },
+  {name:"Venusaur", types:["grass"," poison"] , hieght:"2" },
+  {name:"Charmander", types:"fire" , hieght:".6" },
+  {name:"Charmeleon", types:"fire" , hieght:"1.1" },
+  {name:"Charizard", types:["fire"," flying" ], hieght:"1.7" }];
 
-let pokeinfo = ""
 
-for (let i=0; i < pokemonList.length; i++){
-    if (pokemonList[i].hieght <1) {pokeinfo= pokemonList[i].name," is a ", pokemonList[i].types," type", "<br>", " hieight is ", pokemonList[i].hieght, " meters. Thats small <br>";}
+let pokeinfo = "";
+
+
+for (let i = 0; i <pokemonList.length; i++){
+    if (pokemonList[i].hieght <1)
+     {pokeinfo= pokemonList[i].name," is a ", pokemonList[i].types," type", "<br>", " hieight is ", pokemonList[i].hieght, " meters. Thats small <br>";}
     
 
-    else if (pokemonList[i].hieght >1.5) {pokeinfo=pokemonList[i].name," is a ", pokemonList[i].types," type", "<br>", " hieight is ", pokemonList[i].hieght, " meters. Thats Big <br>";
+    else if (pokemonList[i].hieght >1.5) {pokeinfo=pokemonList[i].name," is a ", pokemonList[i].types," type", "<br>", " hieight is ",pokemonList[i].hieght, " meters. Thats Big <br>";
     }
     
      else {
         pokeinfo=pokemonList[i].name," is a ", pokemonList[i].types," type", "<br>", " hieight is ", pokemonList[i].hieght, " meters. <br>";
-     }
+     };
     
 }
-pokemonList.forEach (pokedetails);
-function pokedetails(){
-    document.write(pokeinfo)
-}
+document.write(pokeinfo)
+
+
+
 
